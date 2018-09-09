cddcecc09e3b6c0849c04b940eae3fc9f7634098
lintonjr <lintonjr@hotmail.com>
2018-09-09 16:49:06 -0400

    Definindo classes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,15 @@
-// var bob = {
-//     _name: "Bob",
-//     _friends: ["Guilherme", "Camilla", "Murillo"],
-//     printFriends: function printFiends() {
-//         // var self = this;
-//         this._friends.forEach(function(f) {
-//             return console.log(self._name + " knows " + f);
-//         });
-//     }
-// };
+class Color {
+    constructor(codeColor, nameColor) {
+        this.codeColor = codeColor;
+        this.nameColor = nameColor;
+    };
+    getColor() {
+        return {
+            codeColor: this.codeColor,
+            nameColor: this.nameColor
+        };
+    };
+}
 
-// bob.printFriends();
-
-
-//CONCEITO DE ARROW FUNCTIONS
-// var color = () => console.log("red");
-
-// color();
-
-var bob = {
-    _name: "Bob",
-    _friends: ["Guilherme", "Camilla", "Murillo"],
-    printFriends: function printFiends() {
-        this._friends.forEach(f =>
-            console.log(this._name + " knows " + f));
-    }
-};
-
-bob.printFriends();
+let red = new Color("red", "RED");
+console.log(red.getColor());
